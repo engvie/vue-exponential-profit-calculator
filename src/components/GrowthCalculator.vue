06e43bf528b2e171c99edd27a1ff390ee8915c01
Albert E <albert.engvie@exaktasoftware.se>
2019-11-02 23:06:20 +0100

    Update growth calculator

--- src/components/GrowthCalculator.vue
+++ src/components/GrowthCalculator.vue
@@ -1,51 +1,91 @@
-'<template>
+<template>
   <div>
-      <form>
-        <div class="form-group">
-          <label>Startsumma:</label>
-          <input type="number" v-model="start" class="form-control" min="1">
-        </div>
-        <div class="form-group">
-          <label>Procent:</label>
-          <input type="number" v-model="percentage" class="form-control" min="1" placeholder="%">
-        </div>
-        <div class="form-group">
-          <label>Steg:</label>
-          <input type="number" v-model="steps" class="form-control" min="1">
-        </div>
-        <div class="form-group">
-          <label>Visnings, stegning:</label>
-          <input type="number" v-model="displaySteps" class="form-control" min="1">
-        </div>
-      </form>
-
-      <div class="chart">
-        <line-chart
-          :chart-data="{
-            labels: result.stepNumbers,
-            datasets: [
-              {
-                label: 'Start '+ start +' | '+ percentage + '% | ' + steps + ' steps',
-                borderColor: '#007bff',
-                fill: false,
-                data: result.chart,
-              },
-            ],
-          }"
-        />
+    <div class="row">
+      <div class="col-lg-9">
+        <form @submit.prevent="save">
+          <div class="row">
+            <div class="col-lg-6">
+              <div class="form-group">
+                <label>Startsumma:</label>
+                <input type="number" v-model="form.start" class="form-control" min="1">
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="form-group">
+                <label>Procent:</label>
+                <input type="number" v-model="form.percentage" class="form-control" min="1" placeholder="%">
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="form-group">
+                <label>Steg:</label>
+                <input type="number" v-model="form.steps" class="form-control" min="1">
+              </div>
+            </div>
+            <div class="col-lg-6">
+              <div class="form-group">
+                <label>Visnings, stegning:</label>
+                <input type="number" v-model="form.displaySteps" class="form-control" min="1">
+              </div>
+            </div>
+            <div class="col-lg-12">
+              <button type="submit" class="btn btn-primary w-100">Spara</button>
+            </div>
+          </div>
+        </form>
       </div>
-
-      <div class="result">
-        <ul>
+      <div class="col-lg-3">
+        <ul class="list-group storage-list">
           <li
-            v-for="(item, i) in result.steps"
-            :key="i"
+            v-for="(item, key) in storage"
+            :key="key"
+            class="list-group-item d-flex justify-content-between"
+            @click="setCurrent(item)"
           >
-            <span class="step-nr">{{ item.step }}:</span> <span class="step-sum">{{ item.sum }}</span>
+            <span><i class="fas fa-dollar-sign"></i> {{ item.start }}</span>
+            <span><i class="fas fa-shoe-prints"></i> {{ item.steps }}</span>
+            <span><i class="fas fa-percentage"></i> {{ item.percentage }}</span>
+            <span class="total">
+              {{ getTotal(item) }}
+            </span>
+            <span class="remove" @click="removeStored(key)""><i class="fas fa-trash-alt"></i></span>
           </li>
         </ul>
       </div>
+    </div>
 
+    <div class="row mt-5">
+      <div class="col-lg-9">
+        <div class="chart">
+          <line-chart
+            :chart-data="{
+              labels: result.stepNumbers,
+              datasets: [
+                {
+                  label: 'Start '+ form.start +' | '+ form.percentage + '% | ' + form.steps + ' steps',
+                  borderColor: '#007bff',
+                  fill: false,
+                  data: result.chart,
+                },
+              ],
+            }"
+          />
+        </div>
+      </div>
+      <div class="col-lg-3">
+        <h3 class="text-center text-primary">{{ total }}</h3>
+        <ul class="list-group">
+          <li
+            v-for="(item, i) in result.steps"
+            :key="i"
+            class="list-group-item d-flex"
+          >
+            <span class="step-nr">{{ item.step }}:</span>
+            <span class="step-sum ml-auto">{{ item.sum }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -58,16 +98,18 @@ export default {
   },
   data: () => {
     return {
-      start: 10000,
-      percentage: 2,
-      steps: 100,
-      displaySteps: 1,
-      current: 0,
+      storage: [],
+      form: {
+        start: 10000,
+        percentage: 2,
+        steps: 100,
+        displaySteps: 1,
+      },
     };
   },
   computed: {
     convertedPercentage: function() {
-      return 1 + parseFloat(this.percentage / 100);
+      return 1 + parseFloat(this.form.percentage / 100);
     },
     result: function() {
       let result = {
@@ -76,9 +118,9 @@ export default {
         stepNumbers: [],
         chart: [],
       };
-      let current = this.start;
-      let displaySteps = parseInt(this.displaySteps) - 1;
-      for (let i = 1; i <= this.steps; i++) {
+      let current = this.form.start;
+      let displaySteps = parseInt(this.form.displaySteps) - 1;
+      for (let i = 1; i <= this.form.steps; i++) {
         current = current * this.convertedPercentage;
         let item = {
           step: i,
@@ -92,17 +134,56 @@ export default {
         
         if (i == 1 || i > displaySteps) {
           result.steps.push(item);
-          displaySteps += this.displaySteps;
+          displaySteps += this.form.displaySteps;
         }
       }
 
       return result;
     },
+    total () {
+      return this.getTotal(this.form);
+    },
   },
   methods: {
     formatNumber: function(num) {
-      return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1\'')
+      return Math.round(num).toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1\'')
     },
+    getStorage () {
+      try {
+        return JSON.parse(window.localStorage.getItem('trading') || '[]');
+      } catch {
+        return [];
+      }
+    },
+    saveStorage (storage) {
+      window.localStorage.setItem('trading', JSON.stringify(storage));
+      this.setStorage();
+    },
+    setStorage() {
+      this.storage = this.getStorage();
+    },
+    removeStored (key) {
+      let storage = this.getStorage();
+      storage.splice(1, 1);
+      this.saveStorage(storage);
+    },
+    setCurrent(form) {
+      console.log(form);
+      this.form = form;
+    },
+    getTotal(data) {
+      return this.formatNumber(data.start * ((1 + parseFloat(data.percentage / 100)) ** data.steps));
+    },
+    save () {
+      let storage = this.getStorage();
+      storage.push(this.form);
+
+      this.saveStorage(storage);
+    },
+  },
+  mounted () {
+    this.setStorage();
+    console.log(this.storage);
   },
 }
 </script>
@@ -111,32 +192,42 @@ export default {
 <style lang="scss" scoped>
 label {
   font-weight: 600;
-  margin: 20px 0;
 }
 input {
   text-align: center;
   font-weight: 600;
   height: 50px;
 }
-.result {
-  ul {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    li {
-      list-style: none;
-      text-align: center;
-      margin: 10px 0;
-      width: 25%;
-      .step-nr {
-        display: inline-block;
-        min-width: 100px;
-        text-align: center;
-      }
-      .step-sum {
-        display: inline-block;
-        font-weight: 600;
-      }
+.storage-list {
+  i {
+    font-size: 0.8em;
+  }
+  li {
+    position: relative;
+    .total {
+      position: absolute;
+      top: 100%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      z-index: 2;
+      background: #fff;
+      font-size: 0.7em;
+      padding: 0 15px;
+      border: 1px solid #ccc;
+      border-top: none;
+    }
+    .remove {
+      position: absolute;
+      top: 50%;
+      left: 100%;
+      transform: translateY(-50%);
+      z-index: 2;
+      background: #fff;
+      padding: 5px;
+      border: 1px solid #ccc;
+      border-left: none;
+      color: red;
+      cursor: pointer;
     }
   }
 }
